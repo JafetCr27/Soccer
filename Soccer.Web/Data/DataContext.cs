0c3b7a7b0ce0cafb258c4cee331c665c062d2b6b
Jafeth Barrera Quesada <jafetbarrera27@gmail.com>
2021-01-22 09:45:30 -0600

    Add Team Controller

---
+++ Soccer.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data.Entities;
+
+namespace Soccer.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+        }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ Soccer.Web/Data/Entities/Team.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Soccer.Web.Data.Entities
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        [MaxLength(100, ErrorMessage = "El campo {0} no permite mas de {1} caracteres.")]
+        [Required]
+        public string Nombre { get; set; }
+        [Display(Name = "Logo")]
+        public string LogoPath { get; set; }
+    }
+}
